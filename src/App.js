3d267d86f55abb56706ef7def1b735820e426658
Kagen Hearn <kl.hearn40@gmail.com>
2021-08-18 16:32:55 -0400

    Added controlled state to text inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { createTextInput, createMultiLineText, createSelectField, createMultipleChoice } from './lib';
+import { createTextInput, createMultiLineText, createSelectField, createMultipleChoice, createCheckboxField, createNumericInput } from './lib';
+import { useState } from 'react';
 
 function App() {
+  const [textValue, setTextValue] = useState("");
+
   return (
     <>
-      <div style={{ width: "600px" }}>{createTextInput({ instructions: "Put in some text here...", placeholder: "Put in some text here..."})}
+      <div style={{ width: "600px" }}>
+        {createTextInput(textValue, setTextValue)}
         {createMultiLineText({ instructions: "Put in some text here...", placeholder: "Put in some text here..." })}
         {createSelectField({choices: ['1', '2', '3']})}
         {createMultipleChoice({ labelPlacement: "top" })}
+        {createCheckboxField()}
+        {createNumericInput()}
       </div>
     </>
   );
--- src/lib/index.js
+++ src/lib/index.js
@@ -5,14 +5,15 @@ const optionDefaults = {
     maxlength: 255,
     required: false,
     size: "m",
-    defaultValue: "",
     placeholder: "",
     instructions: "",
     labelPlacement: "top",
     preStyled: true,
     choices: ['First Choice', 'Second Choice', 'Third Choice'],
     name: "name",
-}
+    min: 0,
+    max: 99,
+};
 
 const globalStylesInput = {
     marginTop: "5px",
@@ -33,7 +34,9 @@ const instructionStyles = {
     backgroundColor: "rgba(0, 0, 0, 0.05)"
 };
 
-export function createTextInput(passedOptions = optionDefaults) {
+export function createTextInput(stateSelector, setStateSelector, passedOptions = optionDefaults) {
+    if(stateSelector === undefined) throw new Error("createTextInput must be given a state selector for React controlled inputs.")
+    if(setStateSelector === undefined) throw new Error("createTextInput must be given a state selector setter for React controlled inputs.")
     const options = {
         ...optionDefaults,
         ...passedOptions
@@ -46,9 +49,10 @@ export function createTextInput(passedOptions = optionDefaults) {
                 <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                     <input
                         type="text"
+                        value={stateSelector}
+                        onChange={(e) => setStateSelector(e.target.value)}
                         style={{ width: options.size === "m" ? "300px" : "450px", maxHeight: "25px", minHeight: "25px" }}
                         required={options.required}
-                        value={options.defaultValue}
                         placeholder={options.placeholder}
                         maxlength={options.maxlength}
                     />
@@ -63,9 +67,10 @@ export function createTextInput(passedOptions = optionDefaults) {
                     <label style={options.preStyled ? labelStyles : {}}>{options.label}</label>
                     <input
                         type="text"
+                        value={stateSelector}
+                        onChange={(e) => setStateSelector(e.target.value)}
                         style={{ width: (options.size === "m" ? "300px" : "450px") }}
                         required={options.required}
-                        value={options.defaultValue}
                         placeholder={options.placeholder}
                         maxlength={options.maxlength}
                     />
@@ -79,9 +84,10 @@ export function createTextInput(passedOptions = optionDefaults) {
                 <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                     <input
                         type="text"
+                        value={stateSelector}
+                        onChange={(e) => setStateSelector(e.target.value)}
                         style={{ width: options.size === "m" ? "300px" : "450px" }}
                         required={options.required}
-                        value={options.defaultValue}
                         placeholder={options.placeholder}
                         maxlength={options.maxlength}
                     />
@@ -280,4 +286,134 @@ export function createMultipleChoice(passedOptions = optionDefaults) {
             </div>
         );
     }
+}
+
+export function createCheckboxField(passedOptions = optionDefaults) {
+    const options = {
+        ...optionDefaults,
+        ...passedOptions,
+    };
+
+
+    if (options.labelPlacement === "top") {
+        return (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <label style={options.preStyled ? labelStyles : {}}>{options.label}</label>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <div>
+                        {options.choices.map(choice => (
+                            <div>
+                                <input
+                                type="checkbox"
+                                value={choice}
+                                name={options.name}
+                                />
+                                <label style={{marginLeft: "5px"}}>{choice}</label>
+                            </div>
+                         ) )}
+                        </div>
+                    <span style={options.preStyled ? instructionStyles : {}}>{options.instructions}</span>
+                </div>
+            </div>
+        );
+    }
+    else if (options.labelPlacement === "left") {
+        return (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <label style={options.preStyled ? labelStyles : {}}>{options.label}</label>
+                    <div>
+                        {options.choices.map(choice => (
+                            <div>
+                                <input
+                                type="checkbox"
+                                value={choice}
+                                name={options.name}
+                                />
+                                <label style={{marginLeft: "5px"}}>{choice}</label>
+                            </div>
+                         ) )}
+                    </div>
+                    <span style={options.preStyled ? instructionStyles : {}}>{options.instructions}</span>
+                </div>
+            </div>
+        );
+    }
+    else if (options.labelPlacement === "right") {
+        return (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <div>
+                        {options.choices.map(choice => (
+                            <div>
+                                <input
+                                type="checkbox"
+                                value={choice}
+                                name={options.name}
+                                />
+                                <label style={{marginLeft: "5px"}}>{choice}</label>
+                            </div>
+                         ) )}
+                    </div>
+                    <label style={options.preStyled ? labelStyles : {}}>{options.label}</label>
+                    <span style={options.preStyled ? instructionStyles : {}}>{options.instructions}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export function createNumericInput(passedOptions = optionDefaults) {
+    const options = {
+        ...optionDefaults,
+        ...passedOptions
+    };
+
+    if (options.labelPlacement === "top") {
+        return (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <label style={options.preStyled ? labelStyles : {}}>{options.label}</label>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <input
+                        type="number"
+                        style={{ width: options.size === "m" ? "100px" : "150px" }}
+                        required={options.required}
+                        placeholder={options.placeholder}
+                    />
+                    <span style={options.preStyled ? instructionStyles : {}}>{options.instructions}</span>
+                </div>
+            </div>
+        );
+    } else if (options.labelPlacement === "left") {
+        return (
+            <div style={{ display: "flex" }}>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <label style={options.preStyled ? labelStyles : {}}>{options.label}</label>
+                    <input
+                        type="number"
+                        style={{ width: options.size === "m" ? "100px" : "150px" }}
+                        required={options.required}
+                        placeholder={options.placeholder}
+                    />
+                    <span style={options.preStyled ? instructionStyles : {}}>{options.instructions}</span>
+                </div>
+            </div>
+        );
+    } else if (options.labelPlacement === "right") {
+        return (
+            <div style={{ display: "flex" }}>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <input
+                        type="number"
+                        style={{ width: options.size === "m" ? "100px" : "150px" }}
+                        required={options.required}
+                        placeholder={options.placeholder}
+                    />
+                    <label style={options.preStyled ? labelStyles : {}}>{options.label}</label>
+                    <span style={options.preStyled ? instructionStyles : {}}>{options.instructions}</span>
+                </div>
+            </div>
+        );
+    }
+
 }
